3a1b7cd1dfbff583337d47d51e545830fc41cdbe
David Smith <david.smith@wirewool.com>
2015-05-19 12:20:59 +0100

    Initial translations

---
+++ language/english/comment_notification_lang.php
@@ -0,0 +1,37 @@
+<?php  if ( ! defined('BASEPATH')) exit('No direct script access allowed');
+
+/**
+ * Install / Uninstall and updates the modules
+ *
+ * @category  User_Notification
+ * @package   Comment_Notification
+ * @author    David Smith <david.smith@wirewool.com>
+ * @copyright 2015 Wirewool Ltd <http://www.wirewool.com>
+ * @license   http://www.wirewool.com/license/
+ * @version   1.0
+ * @link      http://www.wirewool.com/
+ * @see       http://www.wirewool.com/ee/comment_notification
+ */
+$lang = array(
+
+// Required for MODULES page
+'comment_notification_module_name'          => 'Comment Notification',
+'comment_notification_module_description'   => 'Notify the comment author when their comment has been updated',
+
+//----------------------------------------
+
+// Additional Key => Value pairs go here
+'home'                                      => 'Module Home',
+'settings'                                  => 'Module Settings',
+
+// Settings Form
+'setting'                                   => 'Setting Name',
+'value'                                     => 'Setting Value',
+'email_from'                                => 'From Email Address',
+'email_reply'                               => 'Email Reply Address',
+'email_subject'                             => 'Email Subject',
+
+);
+
+/* End of file comment_notification_lang.php */
+/* Location: ./system/expressionengine/third_party/comment_notification/language/english/comment_notification_lang.php */
